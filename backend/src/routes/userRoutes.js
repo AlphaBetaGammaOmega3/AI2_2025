9bac5d5b56dc69e7b172039096666337d4006e19
AlphaBetaGammaOmega3 <danissantos7@gmail.com>
2025-07-08 10:23:19 +0100

    Perfil
    consegues ver o teu perfil e podes editar tbm

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/usersController');
+const isAuthenticated = require('../middleware/isAuthenticated'); // ⬅️ Importa o middleware
 
 router.post('/', controller.create);
-router.get('/', controller.findAll);
-router.get('/:iduser', controller.get);
-router.put('/:iduser', controller.update);
-router.delete('/:iduser', controller.delete);
+router.get('/', isAuthenticated, controller.findAll); // se quiser proteger a listagem
+router.get('/:iduser', isAuthenticated, controller.get); // protege perfil
+router.put('/:iduser', isAuthenticated, controller.update); // ⬅️ ESSENCIAL
+router.delete('/:iduser', isAuthenticated, controller.delete); // opcional
 
 module.exports = router;
