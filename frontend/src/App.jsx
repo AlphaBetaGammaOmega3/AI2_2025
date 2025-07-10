563c462f89c96f68168993cfa8b5cdb13820cc77
AlphaBetaGammaOmega3 <danissantos7@gmail.com>
2025-07-10 12:33:36 +0100

    App.jsx corrigido

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Login from './views/Auth/Login';
 import Register from './views/Auth/CreateAccount';
+import LoginAdmin from './views/Auth/LoginAdmin';
+
 import Home from './views/Cliente/Home';
 import Cart from './views/Cliente/Cart';
 import Perfil from './views/Cliente/Profile';
+
 import AdminDashboard from './views/Admin/HomeAdmin';
-import ProdutoEditar from './views/Admin/EditProuct';
 import AdminProdutos from './views/Admin/AdminProdutos';
-import AdminVendas from './views/Admin/AdminVendas';
 import AdminProdutoCriar from './views/Admin/AdminProdutoCriar';
 import AdminProdutoEditar from './views/Admin/AdminProdutoEditar';
-import ProdutoEditar from './views/Admin/EditProduct';
+import ProdutoEditar from './views/Admin/EditProduct'; // Mantido apenas um
 import ProdutoAdmin from './views/Admin/ProductList';
 import VendasAdmin from './views/Admin/soldProducts';
-import LoginAdmin from './views/Auth/LoginAdmin';
 import ProdutoAdd from './views/Admin/AddProduct';
-
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Cliente */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/profile' element={<Perfil />} />
-        <Route path='/homeAdmin' element = { <AdminDashboard />} />
-        <Route path='/adminprodutos' element={<AdminProdutos />} />
-        <Route path='/admin/produtos/novo' element={<AdminProdutoCriar />} />
-        <Route path='/admin/produtos/editar/:idproduto' element={<AdminProdutoEditar />} />
-        <Route path='/editProduct' element = { <ProdutoEditar />} />
-        <Route path='/adminvendas' element={<AdminVendas />} />
-        {/* Podes adicionar outras rotas aqui no futuro */}
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Perfil />} />
 
-        <Route path='/homeAdmin' element={<AdminDashboard />} />
-        <Route path='/editProduct' element={<ProdutoEditar />} />
-        <Route path='/homeAdmin' element = { <AdminDashboard />} />
-        <Route path='/editProduct' element = { <ProdutoEditar />} />
-        <Route path='/listProducts' element = { <ProdutoAdmin /> } />
-        <Route path='/soldProducts' element = { <VendasAdmin /> } />
-        <Route path='/loginAdmin' element = { <LoginAdmin /> } />
-        <Route path="/AddProduct" element = {<ProdutoAdd /> } />
+        {/* Admin */}
+        <Route path="/homeAdmin" element={<AdminDashboard />} />
+        <Route path="/adminprodutos" element={<AdminProdutos />} />
+        <Route path="/admin/produtos/novo" element={<AdminProdutoCriar />} />
+        <Route path="/admin/produtos/editar/:idproduto" element={<AdminProdutoEditar />} />
+        <Route path="/editProduct" element={<ProdutoEditar />} />
+        <Route path="/listProducts" element={<ProdutoAdmin />} />
+        <Route path="/soldProducts" element={<VendasAdmin />} />
+        <Route path="/loginAdmin" element={<LoginAdmin />} />
+        <Route path="/AddProduct" element={<ProdutoAdd />} />
       </Routes>
     </Router>
   );
