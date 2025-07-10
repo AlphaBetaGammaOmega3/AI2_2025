2157b8ab865039b5bb1053c4be6ec377e3f6ab4c
AlphaBetaGammaOmega3 <danissantos7@gmail.com>
2025-07-10 14:04:27 +0100

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './views/Auth/Login';
 import Register from './views/Auth/CreateAccount';
-import LoginAdmin from './views/Auth/LoginAdmin';
 
 import Home from './views/Cliente/Home';
 import Cart from './views/Cliente/Cart';
@@ -30,7 +29,6 @@ function App() {
         <Route path="/adminprodutos" element={<AdminProdutos />} />
         <Route path="/admin/produtos/novo" element={<AdminProdutoCriar />} />
         <Route path="/admin/produtos/editar/:idproduto" element={<AdminProdutoEditar />} />
-        <Route path="/loginAdmin" element={<LoginAdmin />} />
       </Routes>
     </Router>
   );
