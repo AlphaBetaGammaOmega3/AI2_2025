e7b37226afb3591567b2496c49fcfeaa7af44372
AlphaBetaGammaOmega3 <danissantos7@gmail.com>
2025-07-12 21:17:16 +0100

    .

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "postgres",
-    DB: "loja",
-    dialect: "postgres",
-    PORT: 5432,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-   }
+  HOST: process.env.DB_HOST || "localhost",
+  USER: process.env.DB_USER || "postgres",
+  PASSWORD: process.env.DB_PASSWORD || "postgres",
+  DB: process.env.DB_NAME || "loja",
+  dialect: "postgres",
+  PORT: process.env.DB_PORT || 5432,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
